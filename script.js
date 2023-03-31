6818cc78778a56bb1b131212c3cf8b5ad8d9e039
Adebusayo <adebusayoopeyemi@gmail.com>
2023-03-31 19:43:22 +0530

    mentro web challenge

---
+++ script.js
@@ -0,0 +1,151 @@
+const slideshow = document.querySelectorAll(".slide");
+const names = document.querySelector(".h1");
+const rate = document.querySelector(".rate");
+const rating = document.querySelector(".rating");
+const position = document.querySelector(".title");
+const about = document.querySelector(".text");
+const rightbtn = document.querySelector(".btn-right");
+const leftbtn = document.querySelector(".btn-left");
+const nameTag = document.querySelector(".name-tag");
+const slideHolder = document.querySelector(".slide-holder");
+const centerImg = document.querySelector(".center-img");
+let current = 0;
+let prev = 2;
+let alpha = slideshow.length;
+//select all slides
+//use foreach to loop through all the slides
+//i want the position to change with each click
+//select the next btn
+//add an event listener
+//slide.style.left = `${(indx ) *  15} slide 3
+//slide.style.top = `${(indx + current) +  50} slide 3 //if cuurent is 1
+
+const bottomImg = [
+  "  images/image-jennie.jpg",
+  "images/slide6.png",
+  "images/image-thomas.jpg",
+  "images/image-avatar.png",
+  "images/image-emily.jpg",
+];
+const newHead = [
+  {
+    names: `Prankur Gupta`,
+
+    position: `SDE @Amazon`,
+    about: `I am Prankur Gupta, I am a software development engineer at Amazon.I can guide you throughout your preparation phase. Trust me when I know the exact recipe to crock coding interview rounds of top companies for SDE role.  `,
+  },
+  {
+    names: `Ankita`,
+    
+    position: `ML Engineer @Firework`,
+    about: ` Working my way to build the world's future product. 
+        Machine Learning Engineer, Data and Technology Evangelist I
+        breathe in developing software with intelligence.I have been involved
+        with startups with like DailyHunt, Firework HQ, MagilHub to build the AI
+        end of their products.`,
+  },
+
+  {
+    names: `Keshav Bathla`,
+  
+    position: `SDE-1 @Groww`,
+    about: `  A curious learner, on a way to become a kickass developer who writes
+        Python, Java, Javascript and Go | Software Engineer @Groww | Ex-Software
+        Engineer @Grofers | Tech Consultant | Freelancer | Open Source Developer
+        | Coding Instructor`,
+  },
+  {
+    names: `Saumya Singha`,
+    
+    position: `Software Engineer @Red Hat`,
+    about: ` Engineer @Red Hat | Program Manager'20 @GirlScript | GHCI Scholar | International Open Source Award finalist by Red Hat | Winner SIH, 21U21 Award | Google Connect Winner'19 | Mentor GCI`,
+  },
+  {
+    names: `Saurav Pal`,
+    
+    position: `SDE-2 @Amazon`,
+    about: `I am Senior Software Developer American Express. Have been passionately solving Leetcode, HackerRank, Codechef, Codeforces problems, and enthusiastically solving problems with the analysis of space. `,
+  },
+];
+let numRate = ["4.9", "5.0","5.0","5.0","5.0"]
+let rateArr = [
+  `<span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star-half" aria-hidden="true"></i></span>`,
+  `   <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>`,
+  `   <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>`,
+  `   <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>`,
+  `   <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true" ></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>
+              <span><i class="fa fa-star" aria-hidden="true"></i></span>`,
+];
+let actual;
+let maxAngle;
+const change = (direction) => {
+  // alert("workimg")
+  console.log(slideshow.length);
+  if (direction === "right") {
+    prev++;
+
+    if (current > slideshow.length - 1) {
+      current === 0;
+    }
+    current++;
+  }
+  if (current > newHead.length - 1) {
+    current = 0;
+  } else if (current < 0) {
+    current = newHead.length - 1;
+  } else if (direction === "left") {
+    prev--;
+
+    if (current < 1) {
+      current === slideshow.length;
+    }
+    if (current > newHead.length - 1) {
+      current = 0;
+    } else if (current < 0) {
+      current = newHead.length - 1;
+    }
+    current--;
+  }
+  actual = current * 30;
+  maxAngle = 30 * (slideshow.length - 4);
+  if (actual < maxAngle) {
+    slideHolder.style.transform = `rotate(${actual}deg)`;
+  } else if (actual === 0) {
+    slideHolder.style.transform = `rotate${maxAngle}deg`;
+    current === slideshow.length;
+  } else {
+    slideHolder.style.transform = `rotate(${0}deg)`;
+    current = 0;
+  }
+
+  names.innerText = newHead[current].names;
+  rate.innerText = numRate[current];
+  rating.innerHTML = rateArr[current];
+  position.innerText = newHead[current].position;
+  about.innerText = newHead[current].about;
+  nameTag.innerText = newHead[current].names;
+  centerImg.innerHTML = `<img src="${bottomImg[current]}" alt="center-img">`;
+  centerImg.style.transition = `${50}ms ease-in`;
+};
+
+rightbtn.addEventListener("click", () => change("right"));
+leftbtn.addEventListener("click", () => change("left"));
